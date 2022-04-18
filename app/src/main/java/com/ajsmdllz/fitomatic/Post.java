a69416508ca0f258e6a1e9b549925ee110481a6c
Deni <u7284728@anu.edu,au>
2022-04-18 17:31:11 +1000

    Created User and Post classes

---
+++ app/src/main/java/com/ajsmdllz/fitomatic/Post.java
@@ -0,0 +1,8 @@
+package com.ajsmdllz.fitomatic;
+
+public class Post {
+    // This class will be used to create posts
+    private User author;
+    private String location;
+    private String image;
+}
