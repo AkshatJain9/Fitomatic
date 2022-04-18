481721c9ef93f2c9dc3df7d0ba0383201817b2a0
Shaazaan Majeed <u7283219@anu.edu.au>
2022-04-18 15:19:33 +1000

    Refactor + cleaned profilepic

--- app/src/main/java/com/ajsmdllz/fitomatic/UserProfile.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/UserProfile.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class userProfile extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
