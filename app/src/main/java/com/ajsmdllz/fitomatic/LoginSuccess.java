d8953deb08c9ab1d3743e13ac1fa74eee5371123
Shaazaan Majeed <u7283219@anu.edu.au>
2022-04-18 13:09:40 +1000

    Added profile & post buttons with user profile page

--- app/src/main/java/com/ajsmdllz/fitomatic/LoginSuccess.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/LoginSuccess.java
@@ -1,8 +1,15 @@
 package com.ajsmdllz.fitomatic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class LoginSuccess extends AppCompatActivity {
 
@@ -10,5 +17,10 @@ public class LoginSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+    }
+
+    public void viewProfile(View v) {
+        Intent intent = new Intent(this, userProfile.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ajsmdllz/fitomatic/userProfile.java
@@ -0,0 +1,14 @@
+package com.ajsmdllz.fitomatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
