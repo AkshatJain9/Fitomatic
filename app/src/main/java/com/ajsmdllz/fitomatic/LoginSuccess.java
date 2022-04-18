79d2bc156e045a3cd4f89c243ff9cefa74cf36c5
Deni <u7284728@anu.edu,au>
2022-04-18 23:00:36 +1000

    Implemented "search bar" (Does not work)

--- app/src/main/java/com/ajsmdllz/fitomatic/LoginSuccess.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/LoginSuccess.java
@@ -2,9 +2,13 @@ package com.ajsmdllz.fitomatic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class LoginSuccess extends AppCompatActivity {
 
@@ -12,6 +16,14 @@ public class LoginSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+
+        // Search Bar
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView tvSearchBar = findViewById(R.id.searchBar);
+        tvSearchBar.setQueryHint("Search for something");
+
+        tvSearchBar.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //tvSearchBar.setIconified(false);
     }
 
     public void viewProfile(View v) {
