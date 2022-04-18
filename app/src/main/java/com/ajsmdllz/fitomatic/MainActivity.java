62660ce39a2e041cd9c9ffcd988aebeab970c2f7
Akshat Jain <u7284072@anu.edu.au>
2022-04-18 11:18:54 +1000

    Hardcoded Registration Mechanism - LoginSuccess Entry Point

--- app/src/main/java/com/ajsmdllz/fitomatic/MainActivity.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ajsmdllz.fitomatic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,14 +16,20 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.ajsmdllz.fitomatic.databinding.ActivityMainBinding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -34,4 +45,31 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        FirebaseUser user = mAuth.getCurrentUser();
+//    }
+
+    public void fbinit(View v) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+
+    }
+
+    public void loginUser(View v) {
+
+//        String email = findViewById(R.id.email).toString();
+//        String password = findViewById(R.id.pass).toString();
+        mAuth.signInWithEmailAndPassword("testsign0@gmail.com", "ajsmdllz2100#").addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    startActivity(new Intent(MainActivity.this, LoginSuccess.class));
+                } else {
+                    System.out.println(task.getResult());
+                }
+            }
+        });
+    }
 }
