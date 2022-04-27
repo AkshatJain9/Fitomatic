4323e312737c410bd5a768ca0882361e9e2f6643
Shaazaan Majeed <u7283219@anu.edu.au>
2022-04-28 03:55:03 +1000

    Tokenizer

--- app/src/main/java/com/ajsmdllz/fitomatic/SimpleTokenizer.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/SimpleTokenizer.java
@@ -1,13 +1,45 @@
 package com.ajsmdllz.fitomatic;
 
+import java.util.Scanner;
+
 public class SimpleTokenizer extends Tokenizer {
     private String text;    // save text
     private int pos;        // current position
     private Object current; // save token extracted
 
-    public void setText(String text) {
-        this.text = text;
+    public SimpleTokenizer(String input) {
+        this.text = input.trim();
     }
+
+    public static void main(String[] args) {
+        // Create a scanner to get the user's input.
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+         Continue to get the user's input until they exit.
+         To exit press: Control + D or providing the string 'q'
+         Example input you can try: ((1 + 2) * 5)/2
+         */
+        System.out.println("Provide a test string to be tokenized:");
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+
+            // Check if 'quit' is provided.
+            if (input.equals("q"))
+                break;
+
+            // Create an instance of the tokenizer.
+            Tokenizer tokenizer = new SimpleTokenizer(input);
+
+            // Print all the tokens.
+            while (tokenizer.hasNext()) {
+                System.out.print(tokenizer.current() + " ");
+                tokenizer.next();
+            }
+            System.out.println();
+        }
+    }
+
 
     @Override
     public void next() {
