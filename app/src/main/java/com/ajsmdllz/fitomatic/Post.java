8f36f3a738023e5a75df9a414044a7593d23ec42
Shaazaan Majeed <u7283219@anu.edu.au>
2022-04-28 19:26:28 +1000

    Template Design Pattern for Posts

--- app/src/main/java/com/ajsmdllz/fitomatic/Post.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Post.java
@@ -2,74 +2,13 @@ package com.ajsmdllz.fitomatic;
 
 import java.util.ArrayList;
 
-public class Post {
-    // This class will be used to create and handle posts
-    private User author;
-    private String title;
-    private String location; // As an address
-    private String image;
-    private String description;
-    private ArrayList<Activities> activities;
-    private int likes;
+public abstract class Post {
 
-
-    public Post(User author, String title, String location, String image, String description, ArrayList<Activities> activities, int likes) {
-        this.author = author;
-        this.title = title;
-        this.location = location;
-        this.image = image;
-        this.description = description;
-        this.activities = activities;
-        this.likes = 0;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ArrayList<Activities> getActivities() {
-        return activities;
-    }
-
-    public void setActivities(ArrayList<Activities> activities) {
-        this.activities = activities;
-    }
-
-
+    // These attributes are common among both concrete classes
+    public User author;
+    public String title;
+    public String image;
+    public String description;
+    public ArrayList<User> followers;
+    public int likes;
 }
---
+++ app/src/main/java/com/ajsmdllz/fitomatic/SingleActivity.java
@@ -0,0 +1,58 @@
+package com.ajsmdllz.fitomatic;
+
+public class SingleActivity extends Post {
+
+    // This class will be used to create and handle posts
+    public Activities activity;
+
+
+    public SingleActivity(User author, String title, String image, String description, Activities activity, int likes) {
+        this.author = author;
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.activity = activity;
+        this.likes = likes;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Activities getActivities() {
+        return activity;
+    }
+
+    public void setActivities(Activities activity) {
+        this.activity = activity;
+    }
+
+}
