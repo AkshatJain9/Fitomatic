da08b73b4bdabb27360c79015be13b44cf425d00
Leon Zolati <u7285188@anu.edu.au>
2022-05-02 22:03:11 +1000

    added button that links to the post host activity

--- app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/LargePostFragment.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/LargePostFragment.java
@@ -1,4 +1,4 @@
-package com.ajsmdllz.fitomatic;
+package com.ajsmdllz.fitomatic.Posts.typesOfPosts;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ajsmdllz.fitomatic.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SmallPostFragment#newInstance} factory method to
+ * Use the {@link LargePostFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SmallPostFragment extends Fragment {
+public class LargePostFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class SmallPostFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public SmallPostFragment() {
+    public LargePostFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class SmallPostFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SmallPostFragment.
+     * @return A new instance of fragment LargePostFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SmallPostFragment newInstance(String param1, String param2) {
-        SmallPostFragment fragment = new SmallPostFragment();
+    public static LargePostFragment newInstance(String param1, String param2) {
+        LargePostFragment fragment = new LargePostFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class SmallPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_small_post, container, false);
+        return inflater.inflate(R.layout.fragment_large_post, container, false);
     }
 }
