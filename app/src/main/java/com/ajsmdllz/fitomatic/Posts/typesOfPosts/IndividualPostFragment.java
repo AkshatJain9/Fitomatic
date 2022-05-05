190de5664afadbf61c39dff489986a146a087da2
Deni <u7284728@anu.edu,au>
2022-05-05 15:28:07 +1000

    Fixed post creation error

--- app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/IndividualPostFragment.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/IndividualPostFragment.java
@@ -1,7 +1,5 @@
 package com.ajsmdllz.fitomatic.Posts.typesOfPosts;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,78 +13,26 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.ajsmdllz.fitomatic.FeedAdapter;
-import com.ajsmdllz.fitomatic.Posts.PostCreationSingle;
 import com.ajsmdllz.fitomatic.R;
-import com.ajsmdllz.fitomatic.Search.SearchTokenizer;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link IndividualPostFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class IndividualPostFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+public class IndividualPostFragment extends Fragment implements AdapterView.OnItemSelectedListener{
     private FirebaseAuth mAuth;
     FirebaseFirestore db;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public IndividualPostFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IndividualPostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IndividualPostFragment newInstance(String param1, String param2) {
-        IndividualPostFragment fragment = new IndividualPostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_individual_post, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_individual_post, container,false);
     }
 
     @Override
@@ -99,7 +45,7 @@ public class IndividualPostFragment extends Fragment {
         adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         activity.setAdapter(adapter);
         // Sets listener for the spinner
-        activity.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) getContext());
+        activity.setOnItemSelectedListener(this);
 
         EditText title = getView().findViewById(R.id.createTitleSingle);
         EditText bio = getView().findViewById(R.id.createDescriptionSingle);
@@ -113,6 +59,16 @@ public class IndividualPostFragment extends Fragment {
                 // FIXME -- Need to implement firebase Post stuff here (not sure how to do image handling)
             }
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+//        String selectedActivity = (String) parent.getItemAtPosition(pos);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
