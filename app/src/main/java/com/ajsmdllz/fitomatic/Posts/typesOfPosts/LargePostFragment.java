bcdcbe408ebb339b6570d027809907244fbc7538
Shaazaan Majeed <u7283219@anu.edu.au>
2022-05-06 15:02:12 +1000

    Event multiple activity selection

--- app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/LargePostFragment.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Posts/typesOfPosts/LargePostFragment.java
@@ -1,5 +1,7 @@
 package com.ajsmdllz.fitomatic.Posts.typesOfPosts;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.ajsmdllz.fitomatic.R;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -68,9 +71,87 @@ public class LargePostFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_large_post, container, false);
     }
 
+    // initialize variables for multiple activity selection
+    ArrayList<Integer> activityList = new ArrayList<>();
+    String[] activities = {"Running", "Walking", "Weight Lifting", "Rowing", "Yoga", "Soccer", "Hiking",
+                            "Gymnastics", "AFL", "Tennis", "Rugby", "Surfing", "Golf", "Bowling", "Karate",
+                            "Bouldering", "Rock Climbing", "Cycling", "Mountain Biking", "Swimming",
+                            "Cricket", "Judo", "Tai Quan Dao"};
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // initialise variables
+        TextView popUptextView = getView().findViewById(R.id.multiActivityDropdown);
+        boolean[] selectedActivities = new boolean[activities.length];
+        popUptextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Select Activities");
+                // set dialog non cancelable
+                builder.setCancelable(false);
+
+                builder.setMultiChoiceItems(activities, selectedActivities, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        // check condition
+                        if (b) {
+                            // when checkbox selected add in activity list
+                            activityList.add(i);
+                        } else {
+                            // when checkbox unselected remove position from langList
+                            activityList.remove(Integer.valueOf(i));
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+                        // use for loop
+                        for (int j = 0; j < activityList.size(); j++) {
+                            // concat array value
+                            stringBuilder.append(activities[activityList.get(j)]);
+                            // check condition
+                            if (j != activityList.size() - 1) {
+                                // add comma between elements
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        // set text on textView
+                        popUptextView.setText(stringBuilder.toString());
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // dismiss dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // use for loop
+                        for (int j = 0; j < selectedActivities.length; j++) {
+                            // remove all selection
+                            selectedActivities[j] = false;
+                            // clear activity list
+                            activityList.clear();
+                            // clear text view value
+                            popUptextView.setText("");
+                        }
+                    }
+                });
+                // show dialog
+                builder.show();
+            }
+        });
 
         SeekBar priceBar = getView().findViewById(R.id.priceBar);
         TextView price = getView().findViewById(R.id.price);
@@ -101,5 +182,7 @@ public class LargePostFragment extends Fragment {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+
+        // Multiple Activity Selection
     }
 }
