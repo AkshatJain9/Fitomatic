e0a08dc654350f5b5baff85de0c8a71ec1686201
Shaazaan Majeed <u7283219@anu.edu.au>
2022-05-07 14:08:06 +1000

    Factory method with 3 different post types done

--- app/src/main/java/com/ajsmdllz/fitomatic/Posts/PostFactory.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Posts/PostFactory.java
@@ -1,13 +1,13 @@
 package com.ajsmdllz.fitomatic.Posts;
 
-import com.ajsmdllz.fitomatic.Registration.User;
-
 import java.util.ArrayList;
 
 public class PostFactory {
     public Post createPost(String author, String id, String title, String description, String date, ArrayList<String> activities, String location, String image, ArrayList<String> followers, int price, int max, int likes) {
         if (location.equals(""))
             return new SingleActivity(author, id, title, description, date, activities.get(0), likes);
+        else if (activities.size() == 1)
+            return new SmallGroupActivity(author,id,title,description,date,activities.get(0),location,image,followers,max,likes);
         else
             return new EventActivity(author,id,title,description,date,activities,location,image,followers,price,max,likes);
     }
