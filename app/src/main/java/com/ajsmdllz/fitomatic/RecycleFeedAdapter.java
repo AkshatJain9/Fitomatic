54362766eb94ea21e0b5e65b9f24260d1d79273d
Leon Zolati <u7285188@anu.edu.au>
2022-05-09 22:12:34 +1000

    -Posts now display with different xml files and have all information
    -KNOWN BUG: large group posts are not storing their activities correctly

--- app/src/main/java/com/ajsmdllz/fitomatic/RecycleFeedAdapter.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/RecycleFeedAdapter.java
@@ -1,6 +1,8 @@
 package com.ajsmdllz.fitomatic;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,8 @@ import com.ajsmdllz.fitomatic.Posts.EventActivity;
 import com.ajsmdllz.fitomatic.Posts.Post;
 import com.ajsmdllz.fitomatic.Posts.SingleActivity;
 import com.ajsmdllz.fitomatic.Posts.SmallGroupActivity;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 import java.util.List;
 
@@ -26,6 +30,12 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         this.dataset = dataset;
     }
 
+    /** turns the type of post into a number where 0 -> indiv. 1 -> sml. 2 -> lrg.
+     *  return -1 if the post is not any of the defined types of post
+     *
+     * @param position: the position in the list of the post
+     * @return: the type of post as an int (0 -> indiv. 1 -> sml. 2 -> lrg.)
+     */
     @Override
     public int getItemViewType(int position) {
         if(dataset.get(position) instanceof SingleActivity) {
@@ -39,6 +49,13 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     }
 
 
+    /** Inflates the layout of the Posts that is to say that it gets the right type of
+     * xml file for the post.
+     *
+     * @param parent: yes
+     * @param viewType: the type of Post that it is (0 -> indiv. 1 -> sml. 2 -> lrg.)
+     * @return
+     */
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -56,19 +73,58 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         return null;
     }
 
+
+    /** Used to populate the view holders with the things in the post held in the
+     * list
+     *
+     * @param holder: The view holder to be populated
+     * @param position: the position of the Post in the list that will populate the view
+     */
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        //Populate the Individual Posts
         if(dataset.get(position) instanceof SingleActivity) {
+            ((IndividualViewHolder)holder).getAuthor().setText(dataset.get(position).getAuthor());
             ((IndividualViewHolder)holder).getTextViewTitle().setText(dataset.get(position).getTitle());
             ((IndividualViewHolder)holder).getDescription().setText(dataset.get(position).getDescription());
+            ((IndividualViewHolder)holder).getDate().setText(dataset.get(position).getDate());
+            ((IndividualViewHolder)holder).getActivity().setText(((SingleActivity) dataset.get(position)).getActivity());
+
+            //Populate the Small Group Posts
         }else if (dataset.get(position) instanceof SmallGroupActivity){
             ((SmallViewHolder)holder).getTextViewTitle().setText(dataset.get(position).getTitle());
             ((SmallViewHolder)holder).getDescription().setText(dataset.get(position).getDescription());
+            ((SmallViewHolder)holder).getDate().setText(dataset.get(position).getDate());
+            ((SmallViewHolder)holder).getActivity().setText(((SmallGroupActivity) dataset.get(position)).getActivity());
+            ((SmallViewHolder)holder).getLocation().setText(((SmallGroupActivity) dataset.get(position)).getLocation());
+
+            //Populate the Large Group Posts
         } else if ((dataset.get(position) instanceof EventActivity)){
             ((LargeViewHolder)holder).getTextViewTitle().setText(dataset.get(position).getTitle());
             ((LargeViewHolder)holder).getDescription().setText(dataset.get(position).getDescription());
+            ((LargeViewHolder)holder).getDate().setText(dataset.get(position).getDate());
+            ((LargeViewHolder)holder).getLocation().setText(((EventActivity) dataset.get(position)).getLocation());
+            ((LargeViewHolder)holder).getPrice().setText("$" + ((EventActivity) dataset.get(position)).getPrice() + "pp");
+
+            List<String> activities = ((EventActivity) dataset.get(position)).getActivities();
+            if(activities != null) {
+                for (String a : activities) {
+                    Chip c = new Chip(((LargeViewHolder) holder).getActivities().getContext());
+                    c.setText(a);
+                    c.setCheckable(false);
+                    c.setBackgroundTintList(context.getResources().getColorStateList(R.color.purple));;
+                }
+            }
+            else{
+                System.out.println("SHOULD NOT GET HERE POST SHOULD HAVE ACTIVITIES");
+            }
         }
     }
+
+    /** Returns the number of items in the Recycle View
+     *
+     * @return: the number of items in the recycleView
+     */
 
     @Override
     public int getItemCount() {
@@ -82,15 +138,24 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
      */
 
     public class IndividualViewHolder extends RecyclerView.ViewHolder{
+        private final TextView author;
         private final TextView title;
         private final TextView description;
+        private final TextView date;
+        private final Chip activity;
 
         public IndividualViewHolder(@NonNull View itemView) {
             super(itemView);
+            author = itemView.findViewById(R.id.individualAuthorText);
             title = itemView.findViewById(R.id.individualTitleText);
             description = itemView.findViewById(R.id.individualDescriptionText);
+            date = itemView.findViewById(R.id.individualDateText);
+            activity = itemView.findViewById(R.id.individualActivityChip);
         }
 
+        public Chip getActivity() {return activity;}
+        public TextView getDate() {return date;}
+        public TextView getAuthor() {return author;}
         public TextView getTextViewTitle(){return title;}
         public TextView getDescription(){return description;}
     }
@@ -98,13 +163,23 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     public class SmallViewHolder extends RecyclerView.ViewHolder{
         private final TextView title;
         private final TextView description;
+        private final TextView date;
+        private final Chip activity;
+        private final TextView location;
+
 
         public SmallViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.smallTitleText);
             description = itemView.findViewById(R.id.smallDescriptionText);
+            date = itemView.findViewById(R.id.smallDateText);
+            activity = itemView.findViewById(R.id.smallActivityChip);
+            location = itemView.findViewById(R.id.smallLocationText);
         }
 
+        public TextView getLocation() {return location;}
+        public Chip getActivity() {return activity;}
+        public TextView getDate() {return date;}
         public TextView getTextViewTitle(){return title;}
         public TextView getDescription(){return description;}
     }
@@ -112,13 +187,25 @@ public class RecycleFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     public class LargeViewHolder extends RecyclerView.ViewHolder{
         private final TextView title;
         private final TextView description;
+        private final TextView date;
+        private final ChipGroup activities;
+        private final TextView location;
+        private final TextView price;
 
         public LargeViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.largeTitleText);
             description = itemView.findViewById(R.id.largeDescriptionText);
+            date = itemView.findViewById(R.id.largeDateText);
+            activities = itemView.findViewById(R.id.largeActivityChipGroup);
+            location = itemView.findViewById(R.id.largeLocationText);
+            price = itemView.findViewById(R.id.largePriceText);
         }
 
+        public TextView getPrice() {return price;}
+        public ChipGroup getActivities() {return activities; }
+        public TextView getDate() {return date;}
+        public TextView getLocation() {return location;}
         public TextView getTextViewTitle(){return title;}
         public TextView getDescription(){return description;}
     }
