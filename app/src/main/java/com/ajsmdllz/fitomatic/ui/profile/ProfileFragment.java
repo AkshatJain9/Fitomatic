7e55c3cc55f9951d1a6727c1544cf3f449822bc8
Shaazaan Majeed <u7283219@anu.edu.au>
2022-05-10 20:40:14 +1000

    Image display (need to improve but works)

--- app/src/main/java/com/ajsmdllz/fitomatic/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/ui/profile/ProfileFragment.java
@@ -1,11 +1,14 @@
 package com.ajsmdllz.fitomatic.ui.profile;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,15 +18,26 @@ import androidx.fragment.app.Fragment;
 
 import com.ajsmdllz.fitomatic.MainActivity;
 import com.ajsmdllz.fitomatic.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
     private FirebaseAuth mAuth;
     FirebaseFirestore db;
     private String email;
+    private StorageReference mStorage;
 
 
     @Override
@@ -32,6 +46,7 @@ public class ProfileFragment extends Fragment {
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+        mStorage = FirebaseStorage.getInstance().getReference();
 
         // CREATE ARRAYLIST OF POSTS (Followed)
     }
@@ -59,6 +74,22 @@ public class ProfileFragment extends Fragment {
                 Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Display user profile picture
+        ImageView profilePic = getView().findViewById(R.id.profilePicture);
+        try {
+            final File profileFile = File.createTempFile(email, "jpeg");
+            StorageReference picture = mStorage.child("pfpImages/" + email);
+            picture.getFile(profileFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bits = BitmapFactory.decodeFile(profileFile.getAbsolutePath());
+                    profilePic.setImageBitmap(bits);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Button logout = getView().findViewById(R.id.logoutChip);
         logout.setOnClickListener(new View.OnClickListener() {
