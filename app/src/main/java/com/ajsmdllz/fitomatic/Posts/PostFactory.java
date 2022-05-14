9ecaa643e58ff273989e47978476649eb9daa8ce
Shaazaan Majeed <u7283219@anu.edu.au>
2022-05-14 14:29:19 +1000

    Post Factory bug fix

--- app/src/main/java/com/ajsmdllz/fitomatic/Posts/PostFactory.java
+++ app/src/main/java/com/ajsmdllz/fitomatic/Posts/PostFactory.java
@@ -6,7 +6,7 @@ public class PostFactory {
     public Post createPost(String author, String id, String title, String description, String date, ArrayList<String> activities, String location, ArrayList<String> followers, int price, int max, int likes, ArrayList<String> liked) {
         if (location.equals(""))
             return new SingleActivity(author, id, title, description, date, activities.get(0), likes, liked);
-        else if (activities.size() == 1)
+        else if (price == -1)
             return new SmallGroupActivity(author, id, title, description, date, activities.get(0), location, followers, max, likes, liked);
         else
             return new EventActivity(author, id, title, description, date, activities, location, followers, price, max, likes, liked);
